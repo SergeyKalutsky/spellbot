49a48682e48fb968999f5f50acd5ed848a10e016
Sergey Kalutsky <skalutsky@gmail.com>
2022-08-19 16:28:03 +0300

    checking done

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+word = None
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
@@ -21,9 +22,15 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    defenition = get_word_data()
-    await bot.send_audio(message.from_user.id, open("word.mp3", "rb"), performer="Performer", title="Title")
+    global word
+    if word is not None and word != message.text:
+        await message.answer('Неправильно!')
+        await message.answer(word)
+        
+    defenition, word = get_word_data()
+    await bot.send_audio(message.from_user.id, open("word.mp3", "rb"), performer="Word")
     await message.answer(defenition)
+    await message.answer('_ '*len(word))
 
 
 if __name__ == '__main__':
